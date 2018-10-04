bf753e8f00a23c15204ba1cb7df694cb110a3bbd
Mohamad Damad <mohamad.d007@gmail.com>
2018-10-04 19:21:32 +0330

    WIP

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './styles/index.css';
 import App from './App';
 import axios from 'axios';
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+// axios.defaults.baseURL = 'http://88.99.76.115:5000/api';
 
 if (document.getElementById('datesWrapper')) {
     ReactDOM.render(<App/>, document.getElementById('datesWrapper'));
